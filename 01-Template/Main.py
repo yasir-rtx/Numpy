c72ee29cb02413d46ec0bf7f8759ae3c319ca6cc
yasir-rtx <yelfvankarlenstain@gmail.com>
2022-05-17 14:01:42 +0700

    #1 - Template

---
+++ 01-Template/Main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([1,2,3,4]) # numpy array
+b = [1,2,3,4]           # python list
+
+print(a)
+print(b)
+
+a = a + 1
+b = b + [1]
+
+print("Setelah di tambah elemen 1 :")
+print(a)
+print(b)
